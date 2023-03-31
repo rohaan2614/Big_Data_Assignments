f44b3f0f5045b2d61dbe5b3ca79a0d8e73fc59e1
Rohaan Nadeem <rohaan.nadeem2614@gmail.com>
2023-03-31 11:03:59 -0400

    task 2 error fix

--- assignment-6/programs/task_2.py
+++ assignment-6/programs/task_2.py
@@ -13,13 +13,14 @@ my_db = my_client[f'{db_name}']
 add_info_collection = my_db[collection_name]
 with open(additional_data_file, 'r') as src_file:
     successes = 0
-    id_errors = 0
-    revenue_errors = 0
-    cost_errors = 0
-    distributor_errors = 0
-    rating_errors = 0
+    id_absences = 0
+    revenue_absences = 0
+    cost_absences = 0
+    distributor_absences = 0
+    rating_absences = 0
     insertion_errors = 0
     duplicates_ignored = 0
+    useless_objects = 0 # objects that contain nothing expect the id
     for line in src_file.readlines():
         try:
             # load json
@@ -29,25 +30,27 @@ with open(additional_data_file, 'r') as src_file:
             try:
                 restructured_data['_id'] = int(data['IMDb_ID']['value'][2:])
             except KeyError:
-                id_errors += 1
+                id_absences += 1
             try:
                 restructured_data['revenue'] = float(
                     data['box_office']['value'])
             except KeyError:
-                revenue_errors += 1
+                revenue_absences += 1
             try:
                 restructured_data['cost'] = float(data['cost']['value'])
             except KeyError:
-                cost_errors += 1
+                cost_absences += 1
             try:
                 restructured_data['distributor'] = data['distributorLabel']['value']
             except KeyError:
-                distributor_errors += 1
+                distributor_absences += 1
             try:
                 restructured_data['rating'] = data['MPAA_film_ratingLabel']['value']
             except KeyError:
-                rating_errors += 1
-            if len(restructured_data.keys()) == 5:
+                rating_absences += 1
+            
+            # insert data if there's more info than just the id
+            if len(restructured_data) > 1:
                 try:
                     add_info_collection.insert_one(restructured_data)
                     successes += 1
@@ -55,10 +58,11 @@ with open(additional_data_file, 'r') as src_file:
                     duplicates_ignored += 1
                     continue
                 except Exception as E:
-                    print("Unhandled Insertion Error: ", E)
-                    insertion_errors += 1
+                        print("Unhandled Insertion Error: ", E)
+                        insertion_errors += 1
+            else:
+                useless_objects += 1
         except Exception as E:
             print('Unhandled Exception: ', E)
-    total_errors = id_errors + revenue_errors + cost_errors + \
-        distributor_errors + rating_errors + insertion_errors
-    print(f'Successes : {successes}, \nid_errors: {id_errors}\trevenue_errors: {revenue_errors}\tcost_errors: {cost_errors}\ndistributor_errors: {distributor_errors}\t\t\trating_errors: {rating_errors}\ninsertion_errors : {insertion_errors}\tduplicates_ignored : {duplicates_ignored}\nError %: {round(total_errors*100/(total_errors+successes+duplicates_ignored),3)}\tSuccess %: {round(successes*100/(total_errors+successes+duplicates_ignored),3)}')
+    total_errors = id_absences + insertion_errors
+    print(f'Successes : {successes}, \nid_absences: {id_absences}\trevenue_absences: {revenue_absences}\tcost_absences: {cost_absences}\ndistributor_absences: {distributor_absences}\trating_absences: {rating_absences}\ninsertion_errors : {insertion_errors}\tduplicates_ignored : {duplicates_ignored}\t only_id_rows: {useless_objects}\nError %: {round(total_errors*100/(total_errors+successes+duplicates_ignored),3)}\tSuccess %: {round(successes*100/(total_errors+successes+duplicates_ignored),3)}')
