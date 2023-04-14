c4e6acdad81432bdc1b74c64e2af59730ee36c8d
Rohaan Nadeem <rohaan.nadeem2614@gmail.com>
2023-04-14 19:19:23 -0400

    task 3 completed!

--- assignment-8/task_3.py
+++ assignment-8/task_3.py
@@ -2,21 +2,18 @@
 
 import pymongo
 import argparse
-from config import (db_name, mongo_host, mongo_port, scored_collection, 
-                    task_2_collection as centeroids)
-from utils import get_distances, get_nearest_clusters
-# import copy
+from config import (db_name, mongo_host, mongo_port, scored_collection,
+                    task_2_collection)
+from utils import get_distances, get_nearest_clusters, get_new_centeroids
 
-id_field = '_id'
 
+'''''''''''''''''''''''''''''''''PART 1 : ADD CLUSTER FIELD'''''''''''''''''''''''''''''''''
 
 # parse arguments
 parser = argparse.ArgumentParser(
-    description="This program requires two arguments. -k i.e the number of centorids & -g i.e the genre", )
-# parser.add_argument('-k', type=str, required=True)
+    description="This program requires the argument -g i.e the genre", )
 parser.add_argument('-g', type=str, required=True)
 args = parser.parse_args()
-# number_of_centeroids = int(args.k)
 genre = args.g
 
 
@@ -24,21 +21,54 @@ genre = args.g
 db_address = ('mongodb://' + mongo_host + ':' + mongo_port + '/')
 db_client = pymongo.MongoClient(db_address)
 no_sql_db = db_client.get_database(db_name)
-centeroids = no_sql_db.get_collection(centeroids)
+scored_movies = no_sql_db.get_collection(scored_collection)
+centeroids_collection = no_sql_db.get_collection(task_2_collection)
+centeroids = list(centeroids_collection.find({}))
 
-points = list(centeroids.find({}, {'kMeansNorm':1,}))
-distances = get_distances(points)
-clusters = get_nearest_clusters(distances)
+# get movies of that genre
+points = list(scored_movies.find({'genres': {'$elemMatch': {'$eq': genre}}}, {'kMeansNorm': 1}))
+# print(points,"\n\n\n")
+# assign nearest clusters
+distances = get_distances(points, centeroids)
+# print(distances,"\n\n\n")
+cluster_assignments = get_nearest_clusters(distances)
+print("Clusters:\t",cluster_assignments,"\n\n\n")
+for i in range(len(distances)):
+    data_point_id = cluster_assignments[i][0]
+    query = {"_id": data_point_id}
+    new_value = {"$set": {"cluster": cluster_assignments[i][1]}}
+    scored_movies.update_one(query, new_value)
 
-documents = centeroids.find({})
-i = 0
-print("ID\tCluster\tGenre\tAvg Rating\tstart Year\tTitle")
-for doc in documents:
-    print(f"{doc['_id']}\t   {clusters[i]}\t{genre}\t   {doc['avgrating']}\t       \t{doc['startyear']}\t{doc['title']}")
-    # print(f"ID:\t{doc['_id']}\tCluster:\t{clusters[i]}\tTitle:\t{doc['title']}")
-    query = {"_id" : doc['_id']}
-    new_value = { "$set": {"cluster" : clusters[i]} }
-    centeroids.update_one(query, new_value)
-    i += 1
+# print results
 
-# print("All Done")
+# filter = {'genres': {'$elemMatch': {'$eq': genre}}}
+
+# project = {
+#     'cluster': 1,
+#     'startyear': 1,
+#     'avgrating': 1,
+#     'title': 1
+# }
+
+# sort = list({'cluster': 1}.items())
+# documents = scored_movies.find(filter=filter, projection=project, sort=sort)
+
+# print("ID\tCluster\t\tstart Year\tAvg Rating\tTitle")
+# for doc in documents:
+#     print(f"{doc['_id']}\t   {doc['cluster']}  \t\t  {doc['startyear']}\t\t{doc['avgrating']}\t\t{doc['title']}")
+
+'''''''''''''''''''''''''''''''''PART 2 : UPDATE CENTEROIDS'''''''''''''''''''''''''''''''''
+k = list(centeroids_collection.aggregate(pipeline=[{'$count': 'k'}]))[0]['k']
+new_centeroids = {}
+for i in range(1, k+1):
+    filter = {'genres': {'$elemMatch': {'$eq': genre}},'cluster': i}
+    project={'kMeansNorm': 1}
+    new_centeroids[i] = get_new_centeroids(list(scored_movies.find(filter=filter, projection=project)))
+print(new_centeroids)
+for i in range(1, k+1):
+    data_point_id = i
+    query = {"_id": data_point_id}
+    new_value = {"$set": {"cluster": new_centeroids[i]}}
+    centeroids_collection.update_one(query, new_value)
+
+
--- assignment-8/utils.py
+++ assignment-8/utils.py
@@ -1,7 +1,5 @@
 '''utilities for the code'''
-# libraries
 from math import dist, inf
-import copy
 
 def euclidiean_distance(point_1, point_2):
     if point_1 == point_2:
@@ -9,24 +7,49 @@ def euclidiean_distance(point_1, point_2):
     else:
         return dist(point_1, point_2)
     
-def get_distances(points):
-    points_duplicate = copy.deepcopy(points)
+def get_distances(points, centeroids):
     distances = {}
     for point in points:
         point_coordinates = point['kMeansNorm']
         point_id = point['_id']
-        # point_distance = {point_id : []}
         point_distances = []
 
-        for point_2 in points_duplicate:
-            point_distances.append(euclidiean_distance(point_coordinates, point_2['kMeansNorm']))
+        for point_2 in centeroids:
+            point_distances.append({point_2['_id'] : euclidiean_distance(point_coordinates, point_2['kMeansNorm'])})
         distances[point_id] = point_distances
     return distances
 
-def get_nearest_clusters(distances_dict):
-    clusters = []
-    for doc in distances_dict.keys():
-        smallest_distance = min(distances_dict[doc])
-        nearest_cluster = distances_dict[doc].index(smallest_distance)
-        clusters.append(nearest_cluster)
-    return clusters
+def get_nearest_clusters(distances_array):
+    cluster_assignments = []
+    for document in distances_array.keys():
+        doc_distances = distances_array[document]
+        nearest_cluster, smallest_distance = get_min_across_dicts(doc_distances)
+        cluster_assignments.append((document, nearest_cluster))
+    return cluster_assignments
+
+def get_min_across_dicts(list_of_dicts):
+    smallest_distance = inf
+    smallest_distance_key = None
+    for dict in list_of_dicts:
+        dict_key = list(dict.keys())[0]
+        dict_value = dict[dict_key]
+        if (smallest_distance > dict_value) :
+            smallest_distance = dict_value
+            smallest_distance_key = dict_key
+    return smallest_distance_key, smallest_distance
+
+
+def get_new_centeroids(list_of_points):
+    sum_x = 0
+    sum_y = 0
+    new_x = 0
+    new_y = 0
+    count = 0
+    for point in list_of_points:
+        sum_x += point['kMeansNorm'][0]
+        sum_y += point['kMeansNorm'][1]
+        count += 1
+    if (count != 0) :
+        new_x = sum_x/count
+        new_y = sum_y/count
+    return new_x, new_y        
