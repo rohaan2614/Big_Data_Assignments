874b8cb2293b7638e78b233114cd0bf16025ee60
Rohaan Nadeem <rohaan.nadeem2614@gmail.com>
2023-04-15 18:03:36 -0400

    task 3 bug fix

--- assignment-8/task_3.py
+++ assignment-8/task_3.py
@@ -2,12 +2,11 @@
 
 import pymongo
 import argparse
-from config import (db_name, mongo_host, mongo_port, scored_collection,
-                    task_2_collection)
-from utils import get_distances, get_nearest_clusters, get_new_centeroids
+from config import *
+from utils import *
 
 
-'''''''''''''''''''''''''''''''''PART 1 : ADD CLUSTER FIELD'''''''''''''''''''''''''''''''''
+'''''''''''''''''''''''''''''''''PART 1 : ADD & ASSIGN CLUSTERS'''''''''''''''''''''''''''''''''
 
 # parse arguments
 parser = argparse.ArgumentParser(
@@ -23,52 +22,29 @@ db_client = pymongo.MongoClient(db_address)
 no_sql_db = db_client.get_database(db_name)
 scored_movies = no_sql_db.get_collection(scored_collection)
 centeroids_collection = no_sql_db.get_collection(task_2_collection)
-centeroids = list(centeroids_collection.find({}))
+centeroids = get_centeroids(db=no_sql_db)
 
-# get movies of that genre
-points = list(scored_movies.find({'genres': {'$elemMatch': {'$eq': genre}}}, {'kMeansNorm': 1}))
-# print(points,"\n\n\n")
-# assign nearest clusters
-distances = get_distances(points, centeroids)
-# print(distances,"\n\n\n")
-cluster_assignments = get_nearest_clusters(distances)
-print("Clusters:\t",cluster_assignments,"\n\n\n")
-for i in range(len(distances)):
-    data_point_id = cluster_assignments[i][0]
-    query = {"_id": data_point_id}
-    new_value = {"$set": {"cluster": cluster_assignments[i][1]}}
-    scored_movies.update_one(query, new_value)
+qty_centeroids = list(centeroids_collection.aggregate(pipeline=[{'$count': 'k'}]))[0]['k']
 
-# print results
-
-# filter = {'genres': {'$elemMatch': {'$eq': genre}}}
-
-# project = {
-#     'cluster': 1,
-#     'startyear': 1,
-#     'avgrating': 1,
-#     'title': 1
-# }
-
-# sort = list({'cluster': 1}.items())
-# documents = scored_movies.find(filter=filter, projection=project, sort=sort)
-
-# print("ID\tCluster\t\tstart Year\tAvg Rating\tTitle")
-# for doc in documents:
-#     print(f"{doc['_id']}\t   {doc['cluster']}  \t\t  {doc['startyear']}\t\t{doc['avgrating']}\t\t{doc['title']}")
-
-'''''''''''''''''''''''''''''''''PART 2 : UPDATE CENTEROIDS'''''''''''''''''''''''''''''''''
-k = list(centeroids_collection.aggregate(pipeline=[{'$count': 'k'}]))[0]['k']
-new_centeroids = {}
-for i in range(1, k+1):
-    filter = {'genres': {'$elemMatch': {'$eq': genre}},'cluster': i}
-    project={'kMeansNorm': 1}
-    new_centeroids[i] = get_new_centeroids(list(scored_movies.find(filter=filter, projection=project)))
-print(new_centeroids)
-for i in range(1, k+1):
-    data_point_id = i
-    query = {"_id": data_point_id}
-    new_value = {"$set": {"cluster": new_centeroids[i]}}
+data_points = list(scored_movies.find({'genres': {'$elemMatch': {'$eq': genre}}}, {'kMeansNorm': 1, 'genres' : 1, 'cluster' : 1}))
+# print("Data Points:")
+for data_point in data_points:
+    # find nearest cluster
+    print("\nDatapoint:\t",data_point)
+    distances = get_distances([data_point], centeroids)
+    cluster_assignments = get_nearest_clusters(distances)
+    print("\tCluster Assignment:\t", cluster_assignments)
+    assigned_cluster = cluster_assignments[0][1]
+    # calculate new centeroid coordinates
+    old_coordinates = list(centeroids_collection.find({'_id' : cluster_assignments[0][1]}))[0]['kMeansNorm']
+    new_coordinates = get_new_centeroid([{'kMeansNorm' : old_coordinates}, data_point])
+    print("\tCenteroid Update:\t",old_coordinates,"\t->\t", new_coordinates)
+    # commit updates
+        # centeroid
+    query = {"_id": assigned_cluster}
+    new_value = {"$set": {"kMeansNorm": new_coordinates}}
     centeroids_collection.update_one(query, new_value)
-
-
+        # movie
+    query = {"_id": data_point['_id']}
+    new_value = {"$set": {"cluster": assigned_cluster}}
+    scored_movies.update_one(query, new_value)
--- assignment-8/utils.py
+++ assignment-8/utils.py
@@ -1,5 +1,6 @@
 '''utilities for the code'''
 from math import dist, inf
+from config import *
 
 def euclidiean_distance(point_1, point_2):
     if point_1 == point_2:
@@ -20,11 +21,15 @@ def get_distances(points, centeroids):
     return distances
 
 def get_nearest_clusters(distances_array):
+    # print("Distance Array:")
+    # print("\t", distances_array)
     cluster_assignments = []
+    sum = 0
     for document in distances_array.keys():
         doc_distances = distances_array[document]
-        nearest_cluster, smallest_distance = get_min_across_dicts(doc_distances)
-        cluster_assignments.append((document, nearest_cluster))
+        nearest_cluster, squared_distance = get_min_across_dicts(doc_distances)
+        sum += squared_distance
+        cluster_assignments.append((document, nearest_cluster, squared_distance))
     return cluster_assignments
 
 def get_min_across_dicts(list_of_dicts):
@@ -38,18 +43,108 @@ def get_min_across_dicts(list_of_dicts):
             smallest_distance_key = dict_key
     return smallest_distance_key, smallest_distance
 
-
-def get_new_centeroids(list_of_points):
+def get_new_centeroid(list_cluster_members):
     sum_x = 0
     sum_y = 0
     new_x = 0
     new_y = 0
     count = 0
-    for point in list_of_points:
+    # print("Computing new Centeroid:")
+    for point in list_cluster_members:
         sum_x += point['kMeansNorm'][0]
         sum_y += point['kMeansNorm'][1]
         count += 1
+        # print("\t",sum_x,",\t", sum_y)
     if (count != 0) :
         new_x = sum_x/count
         new_y = sum_y/count
     return new_x, new_y        
+
+def initialize_centeroids(genre, num_of_centeroids, db) :
+    '''Task 2 code converted into a method'''
+    # k random documents associated with genre g
+    pipeline = [{'$match': {'genres': {'$elemMatch': {'$eq': genre}}}}, {'$sample': {'size': num_of_centeroids}}, {'$project': {'kMeansNorm': 1}}]
+
+    k_means_collection = db.get_collection(scored_collection)
+    centeroids = db.get_collection(task_2_collection)
+    results = k_means_collection.aggregate(pipeline=pipeline)
+    modified_results = []
+    i = 1
+    for movie in results:
+        movie['_id'] = i
+        modified_results.append(movie)
+        i += 1
+
+    # delete previous centeroids
+    centeroids.drop()
+    # populate new centeroids
+    centeroids.insert_many(modified_results)
+
+def get_number_clusters(collection, genre):
+    pipeline = [{'$match': {'genres': {'$elemMatch': {'$eq': genre}}}}, {'$count': '#clusters'}]
+    return list(collection.aggregate(pipeline))[0]['#clusters']
+
+def get_clusters(db, genre):
+    '''Returns clusters of the passed genre '''
+    pipeline = [{'$match': {'genres': {'$elemMatch': {'$eq': genre}}}}]
+    collection = db.get_collection(scored_collection)
+    return list(collection.aggregate(pipeline))
+
+def assign_clusters(db, genre):
+    '''Assigns movies to their nearest clusters'''
+    src_collection = db.get_collection(scored_collection)
+    centeroids_collection = db.get_collection(task_2_collection)
+    # get movies of that genre
+    points = list(src_collection.find({'genres': {'$elemMatch': {'$eq': genre}}}, {'kMeansNorm': 1}))
+    centeroids = list(centeroids_collection.find({}))
+    # assign nearest clusters
+    distances = get_distances(points, centeroids)
+    cluster_assignments = get_nearest_clusters(distances)
+    # print("Clusters:\t",cluster_assignments,"\n\n\n")
+    for i in range(len(distances)):
+        data_point_id = cluster_assignments[i][0]
+        query = {"_id": data_point_id}
+        new_value = {"$set": {"cluster": cluster_assignments[i][1], "squared_distance": cluster_assignments[i][2]}}
+        src_collection.update_one(query, new_value)
+
+def update_centeroids(db, genre):
+    '''Updates centeroids by accomodating new datapoints'''
+    src_collection = db.get_collection(scored_collection)
+    centeroids_collection = db.get_collection(task_2_collection)
+    old_centeroids = list(centeroids_collection.find())
+    # print(centeroids)
+    k = list(centeroids_collection.aggregate(pipeline=[{'$count': 'k'}]))[0]['k']
+    new_centeroids = {}
+    for i in range(1, k+1):
+        filter = {'genres': {'$elemMatch': {'$eq': genre}},'cluster': i}
+        project={'kMeansNorm': 1}
+        new_centeroids[i] = get_new_centeroid(list(src_collection.find(filter=filter, projection=project)))
+    # print(new_centeroids)
+    for i in range(1, k+1):
+        data_point_id = i
+        query = {"_id": data_point_id}
+        new_value = {"$set": {"cluster": new_centeroids[i]}}
+        centeroids_collection.update_one(query, new_value)
+    new_centeroids = list(centeroids_collection.find())
+    print('Centeroids updated as:')
+    for i in range(len(old_centeroids)):
+        number = i+1
+        old_centeroid = old_centeroids[i]['kMeansNorm']
+        new_centeroid = new_centeroids[i]['kMeansNorm']
+        print(f"{number}\t{old_centeroid}   ->  {new_centeroid}")
+
+def get_cluster_members(db, cluster_number, genre):
+    '''Returns a list of all the members of that cluster'''
+    filter = {'genres': {'$elemMatch': {'$eq': genre}},'cluster': cluster_number}
+    project={'kMeansNorm': 1, 'cluster' : 1}
+    k_means_collection = db.get_collection(scored_collection)
+    return list(k_means_collection.find(filter=filter, projection=project))
+
+def get_centeroids(db):
+    centeroids_collection = db.get_collection(task_2_collection)
+    return list(centeroids_collection.find({}))
+
+def get_SSE(genre, db):
+    '''Sums over squared distance in the mongo collection'''
+    src_collection = db.get_collection(scored_collection)
+    return list(src_collection.aggregate([{'$match': {'genres': {'$elemMatch': {'$eq': genre}}}}, { '$group': {'_id': 1,'SSE': {'$sum': '$squared_distance'}}}]))[0]['SSE']
